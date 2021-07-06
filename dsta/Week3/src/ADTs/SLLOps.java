c668019f85e8afcdb2e4fdad475ef3d5b931bbbd
cbatuic <cbalaman@uic.edu.ph>
2021-07-06 14:55:29 +0800

    Update SLLOps.java

--- dsta/Week3/src/ADTs/SLLOps.java
+++ dsta/Week3/src/ADTs/SLLOps.java
@@ -36,20 +36,72 @@ public class SLLOps {
     Node head;
     Node tail;
     
+    // constructor - creates new linked list. (partial)
+    SLLOps(){
+        head = tail = null;
+    }
+    
     SLLOps(Node h, Node t){
         head = h;
         tail = t;
     }
     
+    
     @Override
     public String toString(){
-        String str = "";
+        String str = "[";
         Node curr = head;
         while(curr!=null){
-            str = str + curr.data + " ";
+            str = str + curr.data;
             curr = curr.next;
-        }
+            if(curr!=null) str += ", ";
+        }        
+        str = str + "]";
         return str;
+    }
+    
+    public boolean isEmpty(){
+        /*
+        boolean result = false;
+        //logic
+        if(head == null && tail == null) result = true;
+        return result;
+        */
+        
+        return (head==null && tail==null);
+    }
+
+    public void addToTail(SLLOps list, int data) {
+        /*
+        [] head=NULL tail=NULL
+        tmp node
+          +---------+------+
+          |  data   |  ptr |
+          +---------+------+
+        [10]
+        */
+        Node tmp = new Node(data,null);
+        if(list.isEmpty()) {
+            list.head = tmp;
+            list.tail = tmp;
+        }
+        else{
+            list.tail = tmp;
+            // need to traverse (loop) to find the last node
+            // 10 -> 20 -> 30 -> null
+            
+            //dynamic - find the last node            
+            Node curr = list.head;            
+            while(curr!=null){                
+                if(curr.next==null){
+                    // last node found
+                    curr.next = tmp;
+                    break;
+                }
+                curr = curr.next;
+            }           
+
+        }        
     }
     
 }
@@ -65,8 +117,46 @@ class SLLOpsTest{
         n1.next = n2;
         n2.next = n3;
         
-        SLLOps sll = new SLLOps(n1,n3);
-        System.out.printf("%s\n",sll.toString());
+        SLLOps slla = new SLLOps(n1,n3);        
+        //System.out.printf("%s\n",slla.toString());
+        //System.out.printf("Is linked list empty? : %b\n", slla.isEmpty());
         // [10, 20, 30]
+        
+        SLLOps sllb = new SLLOps();        
+        System.out.printf("%s\n",sllb.toString());
+        sllb.addToTail(sllb, 10);
+        System.out.printf("%s\n",sllb.toString());
+        sllb.addToTail(sllb, 20); // [10, 20]
+        System.out.printf("%s\n",sllb.toString());
+        sllb.addToTail(sllb, 30); // [10, 20, 30]
+        System.out.printf("%s\n",sllb.toString());
+        sllb.addToTail(sllb, 40); // [10, 20, 30, 40]
+        System.out.printf("%s\n",sllb.toString());
+        //System.out.printf("Is linked list empty? : %b\n", sllb.isEmpty());
+        
     }
 }
+
+/*
+The linked list ADT needs to support several operations like the following:
+• add – adds a new node containing an info into a linked list
+  addToTail, addToHead, addBeforeNode, addAfterNode
+
+  e.g. addToTail
+  [] (head = null, tail=null)
+  addToTail(sll, 10)
+  [10]
+
+  [10, 20, 30]  (head = 10, tail=30)
+  addToTail(sll, 40)
+  [10, 20, 30, 40]
+  
+
+• find – returns a pointer to the first node containing an info, if any
+• delete – deletes the first node containing an info from a linked list, if any
+
+done:
+• new – creates a new linked list (see constructor)
+• toString – returns a string concatenating the info’s in a linked list
+• isEmpty – determines if a linked list contains nodes or not
+*/
